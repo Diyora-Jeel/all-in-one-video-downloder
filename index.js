905d4452cf5929e92e6c709de99cc470b6b3c796
Diyora-Jeel <jeeldiyora101@gmail.com>
2025-02-17 22:29:38 -0800

    version 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { facebook } = require('another-fb-video-downloader');
+const { tiktokdl } = require('tiktokdl') 
 
 const app = express();
 const PORT = 3000;
@@ -9,25 +9,29 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/"), (req,res) => {
-    res.json("App is running")
+    res.send("App is running")
 }
 
-app.post("/download", (req, res) => {
+app.post("/download", async(req, res) => {
     const url = req.body.url;
-    console.log(url)
-    if (!url || !url.includes("facebook.com")) {
-        return res.status(400).json({ error: "Invalid Facebook URL" });
-    }
+    // console.log(url)
+    // if (!url || !url.includes("facebook.com")) {
+    //     return res.status(400).json({ error: "Invalid Facebook URL" });
+    // }
 
     try {
-        
-        facebook(url,false)
-        .then(videoUrl => 
-            res.json({ videoUrl})
-        )
-        .catch(error => 
-            res.status(404).json({ error })
-        );
+
+        const data = await tiktokdl(url)
+        console.log(data)
+        res.send("Hello")
+
+        // facebook(url,false)
+        // .then(videoUrl => 
+        //     res.json({ videoUrl})
+        // )
+        // .catch(error => 
+        //     res.status(404).json({ error })
+        // );
     } catch (error) {
         res.status(500).json({ error: "Failed to fetch video URL" });
     }
